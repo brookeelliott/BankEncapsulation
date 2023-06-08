10aab5ffdb19e4056ef581c45bd7d0eed34af632
Brooke Elliott <brookeelliott711@gmail.com>
2023-06-07 20:18:27 -0600

    encapsulation exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+
+		public BankAccount()
+		{
+		}
+
+		private double balance = 0;
+		public double depositAmount { get; set; }
+
+		public double Deposit(double depositAmount)
+		{
+			balance = depositAmount;
+			Console.WriteLine("Your money has been deposited.");
+			return balance;
+		}
+
+		public double GetBalance()
+		{
+			Console.WriteLine($"{balance}");
+			return balance;
+		}
+
+
+	}
+
+	
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Would you like to make a deposit?");
+            Console.WriteLine("Enter Any key to continue, enter 'no' to cancel");
+            var startBool = Console.ReadLine();
+
+
+            if (startBool != "no")
+            {
+                Console.WriteLine("Please enter deposit amount.");
+
+
+                BankAccount banking = new BankAccount();
+
+                banking.depositAmount = Convert.ToDouble(Console.ReadLine());
+
+                banking.Deposit(banking.depositAmount);
+
+                Console.WriteLine("Your New Balance is:");
+                banking.GetBalance();
+            }
+
+            Console.WriteLine("Have a great day!");
+
+
+
+            
         }
     }
 }
